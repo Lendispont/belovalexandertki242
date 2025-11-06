bb0933870d32aeeec1742c4a9bbf9a8b77af9355
AlexanderBel <158146051+Lendispont@users.noreply.github.com>
2025-11-06 10:52:39 +0300

    Create Exercise.h

---
+++ Exercise.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "Matrix.h"
+#include "Generator.h"
+#include <memory>
+
+namespace miit::algebra
+{
+    class Exercise
+    {
+    protected:
+        Matrix<int> matrix;
+        std::unique_ptr<Generator> generator;
+
+    public:
+        Exercise(size_t size, std::unique_ptr<Generator> gen);
+        virtual ~Exercise() = default;
+        
+        void fill_matrix();
+        Matrix<int> get_matrix() const;
+        
+        virtual Matrix<int> Task1() = 0;
+        virtual Matrix<int> Task2() = 0;
+        virtual Matrix<int> Task3(const Matrix<int>& D) = 0;
+    };
+}
