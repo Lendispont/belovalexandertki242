e1179ddd8b76339b0cea0284e629208165ba71d1
AlexanderBel <158146051+Lendispont@users.noreply.github.com>
2025-12-18 10:44:42 +0300

    Create ConstantGenerator.h

---
+++ ConstantGenerator.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "Generator.h"
+
+namespace miit::algebra
+{
+    class ConstantGenerator : public Generator
+    {
+    private:
+        int value;
+    public:
+        explicit ConstantGenerator(int value);
+        int generate() override;
+    };
+}
