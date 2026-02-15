f566952b0c92a44103d64346296974d5d90c0dc4
AlexanderBel <158146051+Lendispont@users.noreply.github.com>
2026-02-15 13:18:28 +0300

    Person.h

--- Person.h
+++ Person.h
@@ -2,20 +2,22 @@
 
 class Person {
 protected:
+    int id;
     std::string firstName;
     std::string lastName;
     std::string patronymic;
-    
+
 public:
-    Person(const std::string& firstName, const std::string& lastName, 
+    Person(int id, const std::string& firstName, const std::string& lastName,
            const std::string& patronymic = "");
     virtual ~Person() = default;
-    
+
     virtual std::string getInfo() const;
     virtual std::string getType() const;
-    
+
+    int getId() const;
     std::string getFullName() const;
     std::string getFirstName() const;
     std::string getLastName() const;
     std::string getPatronymic() const;
-}
+};
