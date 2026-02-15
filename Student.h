3cebf4ddbd1e9bf9b6bc704687d1790e311a5915
AlexanderBel <158146051+Lendispont@users.noreply.github.com>
2026-02-15 13:20:32 +0300

    Student.h

--- Student.h
+++ Student.h
@@ -1,24 +1,27 @@
 #include "Person.h"
 #include <string>
+#include <memory>
+
+class Group;
 
 class Student : public Person {
 private:
     std::string recordBookNumber;
-    std::string groupName;
+    std::shared_ptr<Group> group;
     std::string specialization;
-    
+
 public:
-    Student(const std::string& firstName, const std::string& lastName,
+    Student(int id, const std::string& firstName, const std::string& lastName,
             const std::string& patronymic, const std::string& recordBookNumber,
-            const std::string& groupName, const std::string& specialization);
-    
+            const std::shared_ptr<Group>& group, const std::string& specialization);
+
     std::string getInfo() const override;
     std::string getType() const override;
-    
+
     std::string getRecordBookNumber() const;
-    std::string getGroupName() const;
+    std::shared_ptr<Group> getGroup() const;
     std::string getSpecialization() const;
-    
+
     bool matchByRecordBook(const std::string& recordBook) const;
     bool matchByName(const std::string& searchName) const;
-}
+};
