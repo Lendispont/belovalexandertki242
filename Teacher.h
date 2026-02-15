5b88d789f38c9dbd8d7885a6992a9a3860cb062c
AlexanderBel <158146051+Lendispont@users.noreply.github.com>
2026-02-15 13:23:10 +0300

    Teacher.h

--- Teacher.h
+++ Teacher.h
@@ -1,23 +1,27 @@
 #include "Person.h"
 #include <string>
 #include <vector>
+#include <memory>
+
+class Department;
+class Group;
 
 class Teacher : public Person {
 private:
-    std::string department;
-    std::vector<std::string> courses;
-    
+    std::shared_ptr<Department> department;
+    std::vector<std::shared_ptr<Group>> groups;
+
 public:
-    Teacher(const std::string& firstName, const std::string& lastName,
-            const std::string& patronymic, const std::string& department);
-    
+    Teacher(int id, const std::string& firstName, const std::string& lastName,
+            const std::string& patronymic, const std::shared_ptr<Department>& department);
+
     std::string getInfo() const override;
     std::string getType() const override;
-    
-    std::string getDepartment() const;
-    const std::vector<std::string>& getCourses() const;
-    
-    void addCourse(const std::string& course);
-    void removeCourse(const std::string& course);
-    bool teachesCourse(const std::string& course) const;
-}
+
+    std::shared_ptr<Department> getDepartment() const;
+    const std::vector<std::shared_ptr<Group>>& getGroups() const;
+
+    void addGroup(const std::shared_ptr<Group>& group);
+    void removeGroup(const std::shared_ptr<Group>& group);
+    bool teachesInGroup(const std::shared_ptr<Group>& group) const;
+};
