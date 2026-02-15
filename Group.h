48035ba03439319d265c953e691f5b3de4a39ce6
AlexanderBel <158146051+Lendispont@users.noreply.github.com>
2026-02-15 13:29:58 +0300

    Group.h

--- Group.h
+++ Group.h
@@ -1,28 +1,28 @@
-#include <string>
-#include <vector>
-#include <memory>
-
 class Student;
 class Teacher;
+class Department;
 
 class Group {
 private:
     std::string name;
     std::string specialization;
+    std::shared_ptr<Department> department;
     std::vector<std::shared_ptr<Student>> students;
     std::vector<std::shared_ptr<Teacher>> teachers;
-    
+
 public:
-    Group(const std::string& name, const std::string& specialization);
-    
+    Group(const std::string& name, const std::string& specialization,
+          const std::shared_ptr<Department>& department = nullptr);
+
     std::string getName() const;
     std::string getSpecialization() const;
-    
+    std::shared_ptr<Department> getDepartment() const;
+
     void addStudent(const std::shared_ptr<Student>& student);
     void addTeacher(const std::shared_ptr<Teacher>& teacher);
-    
+
     const std::vector<std::shared_ptr<Student>>& getStudents() const;
     const std::vector<std::shared_ptr<Teacher>>& getTeachers() const;
-    
+
     std::vector<std::string> getTeacherNames() const;
 }
