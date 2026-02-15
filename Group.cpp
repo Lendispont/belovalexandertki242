9cfa16104b4fa0f99fe0387dec2e1d15bc7aad7c
AlexanderBel <158146051+Lendispont@users.noreply.github.com>
2026-02-15 13:30:51 +0300

    Group.cpp

--- Group.cpp
+++ Group.cpp
@@ -1,12 +1,15 @@
 #include "Group.h"
 #include "Student.h"
 #include "Teacher.h"
+#include "Department.h"
 
-Group::Group(const std::string& name, const std::string& specialization)
-    : name(name), specialization(specialization) {}
+Group::Group(const std::string& name, const std::string& specialization,
+             const std::shared_ptr<Department>& department)
+    : name(name), specialization(specialization), department(department) {}
 
 std::string Group::getName() const { return name; }
 std::string Group::getSpecialization() const { return specialization; }
+std::shared_ptr<Department> Group::getDepartment() const { return department; }
 
 void Group::addStudent(const std::shared_ptr<Student>& student) {
     students.push_back(student);
