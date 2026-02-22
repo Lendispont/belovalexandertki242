2b648cf23f1b7259a5b50f8727dff04f5ab1e9dd
AlexanderBel <158146051+Lendispont@users.noreply.github.com>
2026-02-22 22:01:58 +0300

    Task2Exercise.cpp

--- Task2Exercise.cpp
+++ Task2Exercise.cpp
@@ -3,42 +3,35 @@
 
 namespace miit::algebra
 {
+    const int Task2Exercise::K = 5;
+
     Task2Exercise::Task2Exercise(size_t size, std::unique_ptr<Generator> gen)
         : Exercise(size, std::move(gen)) {}
 
     void Task2Exercise::execute()
     {
-        fill_matrix();
-        
+
+        matrix.fill_with_generator(*generator);
+
         size_t count_ones = 0;
-        
         for (size_t i = 0; i < matrix.get_size(); ++i)
-        {
             if (contains_digit_one(matrix[i]))
-            {
-                count_ones++;
-            }
-        }
-        
+                ++count_ones;
+
         Matrix<int> result(matrix.get_size() + count_ones);
         size_t result_index = 0;
-        
         for (size_t i = 0; i < matrix.get_size(); ++i)
         {
             if (contains_digit_one(matrix[i]))
-            {
                 result[result_index++] = K;
-            }
             result[result_index++] = matrix[i];
         }
-        
         matrix = std::move(result);
     }
 
     bool Task2Exercise::contains_digit_one(int number) const
     {
         if (number == 0) return false;
-        
         int num = std::abs(number);
         while (num > 0)
         {
