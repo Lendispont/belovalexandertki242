f0b346bc3046ed80c1e7bbb86e6d45ed434a8ffc
AlexanderBel <158146051+Lendispont@users.noreply.github.com>
2026-02-22 21:55:46 +0300

    Matrix.h

--- Matrix.h
+++ Matrix.h
@@ -2,6 +2,7 @@
 #include <memory>
 #include <string>
 #include <stdexcept>
+#include "Generator.h"
 
 namespace miit::algebra
 {
@@ -13,52 +14,49 @@ namespace miit::algebra
         size_t size;
 
     public:
-
         Matrix();
-        explicit Matrix(size_t size);
+        explicit Matrix(const size_t size);
         Matrix(const Matrix& other);
         Matrix(Matrix&& other) noexcept;
 
         ~Matrix() = default;
-        
+
         Matrix& operator=(const Matrix& other);
         Matrix& operator=(Matrix&& other) noexcept;
 
-        Matrix operator<<(int shift) const;
-        Matrix operator>>(int shift) const;
-        
-        T& operator[](size_t index);
-        const T& operator[](size_t index) const;
+        Matrix operator<<(const int shift) const;
+        Matrix operator>>(const int shift) const;
+
+        T& operator[](const size_t index);
+        const T& operator[](const size_t index) const;
 
         size_t get_size() const;
-        void resize(size_t new_size);
-        
+        void resize(const size_t new_size);
+
         std::string to_string() const;
-        
-        void fill_with_generator(class Generator& generator);
+
+        void fill_with_generator(const Generator& generator);
     };
 
     template<typename T>
     Matrix<T>::Matrix() : size(0), data(nullptr) {}
-    
+
     template<typename T>
-    Matrix<T>::Matrix(size_t size) : size(size), data(std::make_unique<T[]>(size)) {}
-    
+    Matrix<T>::Matrix(const size_t size) : size(size), data(std::make_unique<T[]>(size)) {}
+
     template<typename T>
     Matrix<T>::Matrix(const Matrix& other) : size(other.size), data(std::make_unique<T[]>(other.size))
     {
         for (size_t i = 0; i < size; ++i)
-        {
             data[i] = other.data[i];
-        }
     }
-    
+
     template<typename T>
     Matrix<T>::Matrix(Matrix&& other) noexcept : size(other.size), data(std::move(other.data))
     {
         other.size = 0;
     }
-    
+
     template<typename T>
     Matrix<T>& Matrix<T>::operator=(const Matrix& other)
     {
@@ -66,15 +64,13 @@ namespace miit::algebra
         {
             auto new_data = std::make_unique<T[]>(other.size);
             for (size_t i = 0; i < other.size; ++i)
-            {
                 new_data[i] = other.data[i];
-            }
             data = std::move(new_data);
             size = other.size;
         }
         return *this;
     }
-    
+
     template<typename T>
     Matrix<T>& Matrix<T>::operator=(Matrix&& other) noexcept
     {
@@ -86,64 +82,53 @@ namespace miit::algebra
         }
         return *this;
     }
-    
+
     template<typename T>
-    Matrix<T> Matrix<T>::operator<<(int shift) const
+    Matrix<T> Matrix<T>::operator<<(const int shift) const
     {
         Matrix result(size);
         for (size_t i = 0; i < size; ++i)
-        {
             result[(i + shift) % size] = data[i];
-        }
         return result;
     }
-    
+
     template<typename T>
-    Matrix<T> Matrix<T>::operator>>(int shift) const
+    Matrix<T> Matrix<T>::operator>>(const int shift) const
     {
         Matrix result(size);
         for (size_t i = 0; i < size; ++i)
-        {
             result[i] = data[(i + shift) % size];
-        }
         return result;
     }
-    
+
     template<typename T>
-    T& Matrix<T>::operator[](size_t index)
+    T& Matrix<T>::operator[](const size_t index)
     {
         if (index >= size) throw std::out_of_range("Index out of range");
         return data[index];
     }
-    
+
     template<typename T>
-    const T& Matrix<T>::operator[](size_t index) const
+    const T& Matrix<T>::operator[](const size_t index) const
     {
         if (index >= size) throw std::out_of_range("Index out of range");
         return data[index];
     }
-    
+
     template<typename T>
-    size_t Matrix<T>::get_size() const
-    {
-        return size;
-    }
-    
+    size_t Matrix<T>::get_size() const { return size; }
+
     template<typename T>
-    void Matrix<T>::resize(size_t new_size)
+    void Matrix<T>::resize(const size_t new_size)
     {
         auto new_data = std::make_unique<T[]>(new_size);
         size_t min_size = (new_size < size) ? new_size : size;
-        
         for (size_t i = 0; i < min_size; ++i)
-        {
             new_data[i] = data[i];
-        }
-        
         data = std::move(new_data);
         size = new_size;
     }
-    
+
     template<typename T>
     std::string Matrix<T>::to_string() const
     {
@@ -156,17 +141,13 @@ namespace miit::algebra
         result += "]";
         return result;
     }
-    
+
     template<typename T>
-    void Matrix<T>::fill_with_generator(Generator& generator)
+    void Matrix<T>::fill_with_generator(const Generator& generator)
     {
         for (size_t i = 0; i < size; ++i)
         {
-            data[i] = static_cast<T>(generator.generate());
+            data[i] = static_cast<T>(const_cast<Generator&>(generator).generate());
         }
     }
-}
-            }
-        }
-    };
 }
