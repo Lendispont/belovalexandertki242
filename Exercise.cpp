3cee753fe3703660814085593e0e7c77fa983ac3
AlexanderBel <158146051+Lendispont@users.noreply.github.com>
2026-02-22 21:50:16 +0300

    Exercise.cpp

--- Exercise.cpp
+++ Exercise.cpp
@@ -2,13 +2,8 @@
 
 namespace miit::algebra
 {
-    Exercise::Exercise(size_t size, std::unique_ptr<Generator> gen) 
+    Exercise::Exercise(size_t size, std::unique_ptr<Generator> gen)
         : matrix(size), generator(std::move(gen)) {}
-
-    void Exercise::fill_matrix()
-    {
-        matrix.fill_with_generator(*generator);
-    }
 
     Matrix<int> Exercise::get_matrix() const
     {
