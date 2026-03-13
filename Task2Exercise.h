40b74c6487f192677e1047c33bf42a3dcc9293e9
AlexanderBel <158146051+Lendispont@users.noreply.github.com>
2026-03-13 14:03:18 +0300

    Task2Exercise.h

--- Task2Exercise.h
+++ Task2Exercise.h
@@ -6,8 +6,8 @@ namespace miit::algebra
     class Task2Exercise : public Exercise
     {
     private:
+        static const int K;
         bool contains_digit_one(int number) const;
-        static const int K = 5;
         
     public:
         Task2Exercise(size_t size, std::unique_ptr<Generator> gen);
