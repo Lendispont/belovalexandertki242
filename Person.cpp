371db42b1262cd4b67cd9bc496b3cd4f890264e4
AlexanderBel <158146051+Lendispont@users.noreply.github.com>
2026-03-13 14:24:33 +0300

    Person.cpp

--- Person.cpp
+++ Person.cpp
@@ -1,8 +1,43 @@
 #include "Person.h"
+#include <stdexcept>
+#include <algorithm>
+
+int Person::nextId = 1;
+std::set<int> Person::existingIds;
+
+bool Person::isIdUnique(int id) {
+    return existingIds.find(id) == existingIds.end();
+}
+
+int Person::generateUniqueId() {
+    while (!isIdUnique(nextId)) {
+        ++nextId;
+    }
+    int newId = nextId;
+    ++nextId;
+    return newId;
+}
+
+Person::Person(const std::string& firstName, const std::string& lastName,
+               const std::string& patronymic)
+    : id(generateUniqueId()), firstName(firstName), lastName(lastName), patronymic(patronymic) {
+    existingIds.insert(id);
+}
 
 Person::Person(int id, const std::string& firstName, const std::string& lastName,
                const std::string& patronymic)
-    : id(id), firstName(firstName), lastName(lastName), patronymic(patronymic) {}
+    : id(id), firstName(firstName), lastName(lastName), patronymic(patronymic) {
+    
+    if (!isIdUnique(id)) {
+        throw std::invalid_argument("Person with ID " + std::to_string(id) + " already exists!");
+    }
+    
+    existingIds.insert(id);
+    
+    if (id >= nextId) {
+        nextId = id + 1;
+    }
+}
 
 std::string Person::getInfo() const {
     return getFullName() + " (ID: " + std::to_string(id) + ")";
